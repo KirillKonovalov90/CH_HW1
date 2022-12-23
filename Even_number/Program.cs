28ecd35a002497a4a3aaa8918b039d29e2dcd905
Kirill <wakened90@gmail.com>
2022-12-23 13:29:32 +0300

    program number are even working

---
+++ Even_number/Program.cs
@@ -0,0 +1,13 @@
+﻿int number;
+
+Console.Write("Введите число: ");
+number = int.Parse(Console.ReadLine()!);
+
+if (number % 2 == 0)
+{
+    Console.Write("Число чётное");
+}
+else
+{
+    Console.Write("Число нечётное");
+}
