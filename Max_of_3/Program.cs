101c7ed9158e6b619644398ca5d164f67e415186
Kirill <wakened90@gmail.com>
2022-12-23 13:26:34 +0300

    program max of 3 working

---
+++ Max_of_3/Program.cs
@@ -0,0 +1,24 @@
+﻿int number1, number2, number3, max;
+
+Console.Write("Введите первое число: ");
+number1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+number2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите третье число: ");
+number3 = int.Parse(Console.ReadLine()!);
+
+max = number1;
+
+if (number2 > max)
+{
+    max = number2;
+}
+
+if (number3 > max)
+{
+    max = number3;
+}
+
+Console.Write($"Максимум равен = {max}");
