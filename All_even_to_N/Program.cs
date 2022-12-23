83c49a4f77a4f8f40f3f34e42b1990a6dc75cd3e
Kirill <wakened90@gmail.com>
2022-12-23 13:34:01 +0300

    program all even to N are working

--- All_even_to_N/Program.cs
+++ All_even_to_N/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿int N, i;
+
+Console.Write("Введите число N: ");
+N = int.Parse(Console.ReadLine()!);
+
+// тут, конечно, явно не общий случай, а только при условии подсчёта чётных от 1
+
+i = 2; 
+
+while (i <= N)
+{
+    Console.Write($"{i} ");
+    i += 2;
+}
