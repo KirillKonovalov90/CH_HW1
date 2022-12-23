42a0abcf377e5eb6c6335f50f8dffa32a89ec40f
Kirill <wakened90@gmail.com>
2022-12-23 13:16:38 +0300

    program max of 2 working

--- Max_of_2/Program.cs
+++ Max_of_2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number1, number2;
+
+Console.Write("Введите первое число: ");
+number1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+number2 = int.Parse(Console.ReadLine()!);
+
+if (number1 > number2)
+{
+    Console.Write($"Максимальное число = {number1}");
+}
+else
+{
+    Console.Write($"Максимальное число = {number2}");
+}
